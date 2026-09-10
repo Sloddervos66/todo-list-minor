62a23e0835e328332b9c0b6addb3f90587341ef1
Sloddervos66 <sloddervos66@gmail.com>
2026-09-10 22:29:14 +0200

    Simple to do list with persistant state

---
+++ TodoList/Components/Classes/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Components.Classes;
+
+public sealed class Item
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    
+    [Required(ErrorMessage = "Title is required")]
+    public string Title { get; set; } = string.Empty;
+    
+    [DateNotInPast(ErrorMessage = "The date cannot be in the past or today.")]
+    public DateTime When { get; set; } = DateTime.Today;
+    
+    public bool Completed { get; set; }
+}
