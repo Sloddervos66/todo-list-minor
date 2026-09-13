19dfd27dfe8402e1598d525521110d537eec21b2
Sloddervos66 <sloddervos66@gmail.com>
2026-09-13 12:53:29 +0200

    Remove the need for email

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -34,14 +34,13 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
     {
-        options.SignIn.RequireConfirmedAccount = true;
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
     })
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 builder.Services
     .AddRepositories()
