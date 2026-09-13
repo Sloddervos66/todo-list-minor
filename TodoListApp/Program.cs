d41e668589a715bce48496e28fe2c18e01d0880b
Sloddervos66 <sloddervos66@gmail.com>
2026-09-13 15:26:19 +0200

    Logout fix

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -70,5 +70,10 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+app.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
+{
+    await signInManager.SignOutAsync();
+    return Results.LocalRedirect("/Account/Login");
+});
 
 app.Run();
